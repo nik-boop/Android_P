2cd8b77170515b7e392358612d20fe97b99bdacc
Nikita <nakoryakin02@gmail.com>
2022-02-18 11:14:20 +0300

    Service

--- Service/app/src/main/java/com/example/service/MainActivity.java
+++ Service/app/src/main/java/com/example/service/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.service.MyService.MyBinder;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
@@ -10,16 +11,16 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.lang.reflect.Type;
+
 public class MainActivity extends AppCompatActivity {
 
     MyService myService;
-    boolean service_connected;
-    final String ACTIVITY = "MainActivity";
+    int service_flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,79 +32,69 @@ public class MainActivity extends AppCompatActivity {
             public void onServiceConnected(ComponentName className, IBinder service) {
                 MyBinder binder = (MyBinder) service;
                 myService = binder.getService();
-                service_connected = true;
             }
 
             @Override
             public void onServiceDisconnected(ComponentName arg0) {
-                service_connected = false;
+
             }
         };
-
+        service_flag = 0;
         Button btnStartService = findViewById(R.id.btnStartService);
         Button btnBindService = findViewById(R.id.btnBindService);
-        Button btnKillBindService = findViewById(R.id.btnKillBindService);
-        Button btnBindServiceGenNumber = findViewById(R.id.btnBindGenNumber);
-        Button btnStopService = findViewById(R.id.btnStopService);
-        Intent service_intent = new Intent(this, MyService.class);
+        Button btnKillService = findViewById(R.id.btnKillService);
+        Button btnGoService = findViewById(R.id.btnGoService);
         Intent start_service = new Intent(MainActivity.this, MyService.class);
 
         btnStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 startService(start_service);
-            }
-        });
+                service_flag = 1;
 
-        btnStopService.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                stopService(start_service);
             }
         });
 
         btnBindService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bindService(service_intent, mConnection, Context.BIND_AUTO_CREATE);
+                bindService(start_service, mConnection, Context.BIND_AUTO_CREATE);
+                service_flag = -1;
+
             }
         });
 
-        btnKillBindService.setOnClickListener(new View.OnClickListener() {
+        btnKillService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (service_connected) {
-                    unbindService(mConnection);
-                    service_connected = false;
+                if (service_flag == 1){
+                    stopService(start_service);
+                    service_flag = 0;
                 }
+                else if (service_flag == -1){
+                    unbindService(mConnection);
+                    service_flag = 0;
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Сервис не запущен", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
-
-        btnBindServiceGenNumber.setOnClickListener(new View.OnClickListener() {
+        btnGoService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int number = myService.MyService();
-                Toast.makeText(MainActivity.this, "Number: " + number, Toast.LENGTH_SHORT).show();
+                if (myService != null){
+                    int number = myService.MyService();
+                    Toast.makeText(MainActivity.this, "Completed " + number, Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Сервис не вызван ", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
     };
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(ACTIVITY, "onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(ACTIVITY, "onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(ACTIVITY, "onStop()");
-    }
 }
--- Service/app/src/main/java/com/example/service/MyService.java
+++ Service/app/src/main/java/com/example/service/MyService.java
@@ -8,10 +8,10 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
-import java.util.Random;
-
 
 public class MyService extends Service {
+
+    final String DEBUG = "SERVICE";
 
     public class MyBinder extends Binder {
         MyService getService() {
@@ -19,29 +19,18 @@ public class MyService extends Service {
         }
     }
 
-    final IBinder mBinder = new MyBinder();
-    final String DEBUG = "SERVICE";
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        Log.d(DEBUG, "Service binded");
-        return mBinder;
-    }
-
-    public void onRebind(Intent intent) {
-        super.onRebind(intent);
-        Log.d(DEBUG, "MyService onRebind");
-    }
-
-    public boolean onUnbind(Intent intent) {
-        Log.d(DEBUG, "MyService onUnbind");
-        return super.onUnbind(intent);
-    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d(DEBUG, "Service has started");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        final IBinder mBinder = new MyBinder();
+        Log.d(DEBUG, "Service binded");
+        return mBinder;
     }
 
     @Override
@@ -56,14 +45,22 @@ public class MyService extends Service {
         Log.d(DEBUG, "Service has been destroyed!");
     }
 
-    public int MyService() {
-        Random generator = new Random();
-        int random_number = generator.nextInt(10000);
-        while (random_number < 9999) {
-            random_number = generator.nextInt(10000);
-            Log.d(DEBUG, "RandomNumber: " + random_number);
-        }
-        return random_number;
+    public boolean onUnbind(Intent intent) {
+        Log.d(DEBUG, "MyService onUnbind");
+        return super.onUnbind(intent);
+    }
 
+    public int MyService() {
+
+        int number = 0;
+        while (number < 9999) {
+            number ++;
+            Log.d(DEBUG, "Number: " + number);
+        }
+        return 0;
+
+    }
+    public int MS(){
+        return 1;
     }
 }
