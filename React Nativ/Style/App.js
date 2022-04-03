18f540368286fdfe65367e622b389b9692dfc14a
Nikita <nakoryakin02@gmail.com>
2022-04-03 12:40:17 +0300

    Stile

--- React Nativ/Style/App.js
+++ React Nativ/Style/App.js
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { Card } from 'react-native-paper';
 import Constants from 'expo-constants';
 
