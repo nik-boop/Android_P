86a246c8d059798851d0bc83bafa5f91099065f3
Nikita <nakoryakin02@gmail.com>
2022-04-18 10:14:11 +0300

    MapOSM

--- React-Nativ/Maps/App.js
+++ React-Nativ/Maps/App.js
@@ -1,72 +1,62 @@
-import React, { Component } from 'react';
-import {View,StyleSheet,StatusBar,Image,Dimensions} from 'react-native';
-import {Button,Container,Header,Left,Right,Icon,Text,Radio } from 'native-base';
-import MapView ,{ MAP_TYPES, PROVIDER_DEFAULT,UrlTile } from 'react-native-maps';
+import * as React from 'react';
+import MapView, {Callout, Circle, Marker, UrlTile} from 'react-native-maps';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
 
-const { width, height } = Dimensions.get('window');
+export default function App() {
+    const [pin, setPin] = React.useState({
+        latitude: 37.78825,
+        longitude: -122.4324
+    })
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
 
-const ASPECT_RATIO = width / height;
-const LATITUDE = 22.720555;
-const LONGITUDE = 75.858633;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
-class OpenStreetMapScreen extends React.Component {
-    static navigationOptions = {
-        drawerLabel: 'OpenStreetMap'
-    };
-    constructor(props) {
-        super(props);
-        this.state = {
-            region: {
-                latitude: LATITUDE,
-                longitude: LONGITUDE,
-                latitudeDelta: LATITUDE_DELTA,
-                longitudeDelta: LONGITUDE_DELTA,
-            },
-        };
-    }
-    get mapType() {
-        return this.props.provider === PROVIDER_DEFAULT ? MAP_TYPES.STANDARD : MAP_TYPES.NONE;
-    }
-    render() {
-        return (
-            <Container>
-                <Header>
-                    <Left style={{ flexDirection: 'row' }}>
-                        <Icon onPress={() => this.props.navigation.openDrawer()} name="md-menu" style={{ color: 'white', marginRight: 15 }} />
-                    </Left>
-                    <View style={{alignItems:'center',justifyContent:'center'}}>
-                        <Text style={{ color: 'white' }} >OpenStreetMap</Text>
-                    </View>
-                    <Right>
-                        <Icon name="md-cart" style={{ color: 'white' }} />
-                    </Right>
-                </Header>
-                <View >
-                    <MapView
-                        region={this.state.region}
-                        provider={null}
-                        mapType={this.mapType}
-                        rotateEnabled={false}
-                        style={{flex: 1}}
-                        style={styles.map}
-                        showsUserLocation>
-                        <UrlTile
-                            urlTemplate="http://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
-                            maximumZ={19}
-                        />
-                    </MapView>
-                </View>
-            </Container>
-        );
-    }
+            >
+                <Marker
+                    coordinate={{
+                        latitude: 37.78825,
+                        longitude: -122.4324
+                    }}
+                    draggable={true}
+                    onDragStart={(e) => {
+                        console.log("Drag start", e.nativeEvent.coordinate)
+                    }}
+                    onDragEnd={(e) => {
+                        console.log("Drag start", e.nativeEvent.coordinate);
+                        setPin({
+                            latitude: e.nativeEvent.coordinate.latitude,
+                            longitude: e.nativeEvent.coordinate.longitude
+                        })
+                    }}
+                >
+                    <Callout>
+                        <Text>I'm here</Text>
+                    </Callout>
+                </Marker>
+                <Circle
+                    center={pin}
+                    radius={1000}/>
+            </MapView>
+        </View>
+    );
 }
-export default OpenStreetMapScreen
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     map: {
-        width: 400,
-        height: 800,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
 });
