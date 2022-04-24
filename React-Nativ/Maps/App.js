84e05b8ba92988f16ff71137054a1cd15f01bf49
Nikita <nakoryakin02@gmail.com>
2022-04-24 14:16:09 +0300

    MapOSM

--- React-Nativ/Maps/App.js
+++ React-Nativ/Maps/App.js
@@ -56,7 +56,7 @@ export default function App() {
                 )}
                 <Marker
                     coordinate={location}
-                    title={"Start point"}
+                    title={"User"}
                     draggable={true}
                     pinColor={'green'}
                     onDragStart={
