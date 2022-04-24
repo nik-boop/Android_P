877550c9b9578987aa1b658d536cd61f4027626f
Nikita <nakoryakin02@gmail.com>
2022-04-24 13:16:16 +0300

    MapOSM

--- MapOSM/app/src/main/java/com/example/maposm/MainActivity.java
+++ MapOSM/app/src/main/java/com/example/maposm/MainActivity.java
@@ -7,22 +7,43 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.osmdroid.views.overlay.simplefastpoint.LabelledGeoPoint;
+import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlay;
+import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlayOptions;
+import org.osmdroid.views.overlay.simplefastpoint.SimplePointTheme;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private MapView map = null;
-
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
 
     @Override
@@ -32,9 +53,19 @@ public class MainActivity extends AppCompatActivity {
         Context ctx = this.getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 
-        map = findViewById(R.id.mapview);
+        map = findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
-        map.getController().setZoom(18.0);
+
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setCenter(startPoint);
+
+
 
         requestPermissionsIfNecessary(new String[]{
                 Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.INTERNET
@@ -42,19 +73,124 @@ public class MainActivity extends AppCompatActivity {
         map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
         map.setMultiTouchControls(true);
 
+        RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(this, map);
+        mRotationGestureOverlay.setEnabled(true);
+        map.setMultiTouchControls(true);
+        map.getOverlays().add(mRotationGestureOverlay);
 
-        CompassOverlay compassOverlay = new CompassOverlay(this, map);
+
+        MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
+        mLocationOverlay.enableMyLocation();
+        map.getOverlays().add(mLocationOverlay);
+
+
+        CompassOverlay compassOverlay = new CompassOverlay(this, new InternalCompassOrientationProvider(this), map);
         compassOverlay.enableCompass();
         map.getOverlays().add(compassOverlay);
 
-        GeoPoint point = new GeoPoint(45.845557, 26.170010);
+
+        MinimapOverlay mMinimapOverlay = new MinimapOverlay(this, map.getTileRequestCompleteHandler());
+        mMinimapOverlay.setWidth(250);
+        mMinimapOverlay.setHeight(250);
+        //optionally, you can set the minimap to a different tile source
+        //mMinimapOverlay.setTileSource(....);
+        map.getOverlays().add(mMinimapOverlay);
 
         Marker startMarker = new Marker(map);
-        startMarker.setPosition(point);
+        startMarker.setPosition(startPoint);
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+        startMarker.setTitle("Start point");
+
         map.getOverlays().add(startMarker);
 
-        map.getController().setCenter(point);
+
+
+
+        ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+        items.add(new OverlayItem("Title", "Description", new GeoPoint(0.0d,0.0d))); // Lat/Lon decimal degrees
+
+        //the overlay
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+                        //do something
+                        return true;
+                    }
+                    @Override
+                    public boolean onItemLongPress(final int index, final OverlayItem item) {
+                        return true;
+                    }
+                }, this);
+        mOverlay.setFocusItemsOnTap(true);
+
+        map.getOverlays().add(mOverlay);
+
+
+        /*
+        List<IGeoPoint> points = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            points.add(new LabelledGeoPoint(37 + Math.random() * 5, -8 + Math.random() * 5
+                    , "Point #" + i));
+        }
+
+        // wrap them in a theme
+        SimplePointTheme pt = new SimplePointTheme(points, true);
+
+        // create label style
+        Paint textStyle = new Paint();
+        textStyle.setStyle(Paint.Style.FILL);
+        textStyle.setColor(Color.parseColor("#0000ff"));
+        textStyle.setTextAlign(Paint.Align.CENTER);
+        textStyle.setTextSize(24);
+
+        // set some visual options for the overlay
+        // we use here MAXIMUM_OPTIMIZATION algorithm, which works well with >100k points
+        SimpleFastPointOverlayOptions opt = SimpleFastPointOverlayOptions.getDefaultStyle()
+                .setAlgorithm(SimpleFastPointOverlayOptions.RenderingAlgorithm.MAXIMUM_OPTIMIZATION)
+                .setRadius(7).setIsClickable(true).setCellSize(15).setTextStyle(textStyle);
+
+        // create the overlay with the theme
+        final SimpleFastPointOverlay sfpo = new SimpleFastPointOverlay(pt, opt);
+
+        // onClick callback
+        sfpo.setOnClickListener(new SimpleFastPointOverlay.OnClickListener() {
+            @Override
+            public void onClick(SimpleFastPointOverlay.PointAdapter points, Integer point) {
+                Toast.makeText(map.getContext()
+                        , "You clicked " + ((LabelledGeoPoint) points.get(point)).getLabel()
+                        , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // add overlay
+        map.getOverlays().add(sfpo);
+
+         */
+
+
+    }
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 
     @Override
--- React-Nativ/Maps/App.js
+++ React-Nativ/Maps/App.js
@@ -1,36 +1,176 @@
 import * as React from 'react';
-import MapView, {Callout, Circle, Marker, UrlTile} from 'react-native-maps';
-import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import MapView, {Callout, Circle, Marker, Polygon, Polyline, UrlTile} from 'react-native-maps';
+import {StyleSheet, Text, View, Dimensions, Button} from 'react-native';
+import {useState} from "react";
+
 
 export default function App() {
 
+    const [location, setLocation] = useState(
+        {
+            latitude: 37.78825,
+            longitude: -122.4324
+        }
+    );
+    const [lines, setLines] = useState([]);
+
+    const [arraylist, setArr] = useState([]);
+    let last_coords = {};
+    const drawLine = () => {
+        let lines = arraylist.map(item => (item.coords));
+        lines.push(location)
+        setLines(lines)
+    }
+    const clearLine = () => {
+        setLines([])
+    }
+    const clearMarker = () => {
+        setArr([])
+        setLines([])
+    }
     return (
         <View style={styles.container}>
-            <Text>
-                Maps
-            </Text>
+
+            <Text style={styles.title_map}> Maps </Text>
+
             <MapView
                 style={styles.map}
+                provider={null}
+                mapType={"none"}
+                showsMyLocationButton={true}
+                showsUserLocation={true}
             >
-                <MapView.UrlTile
-                    urlTemplate='http://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                />
-            </MapView>
 
+                <UrlTile
+                    //urlTemplate="http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    urlTemplate="https://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+                    maximumZ={20}
+                    zIndex={0}
+                />
+                {arraylist.map((items) =>
+                    <Marker
+                        key={items.title}
+                        coordinate={items.coords}
+                        title={"Point#" + items.title}
+                    />
+                )}
+                <Marker
+                    coordinate={location}
+                    title={"Start point"}
+                    draggable={true}
+                    pinColor={'green'}
+                    onDragStart={
+                        e => {
+                            last_coords = {
+                                latitude: location.latitude,
+                                longitude: location.longitude
+                            };
+                            if (arraylist.length === 0) {
+                                setArr(
+                                    arraylist.concat(
+                                        {
+                                            coords: last_coords,
+                                            title: 1
+                                        }
+                                    )
+                                );
+                            } else {
+                                setArr(
+                                    arraylist.concat(
+                                        {
+                                            coords: last_coords,
+                                            title: arraylist[arraylist.length - 1].title + 1
+                                        }
+                                    )
+                                );
+                            }
+                        }
+                    }
+                    onDragEnd={
+                        e => {
+                            setLocation(
+                                {
+                                    latitude: e.nativeEvent.coordinate.latitude,
+                                    longitude: e.nativeEvent.coordinate.longitude
+                                }
+                            );
+                        }
+                    }
+
+                />
+
+                <Circle
+                    radius={1000}
+                    center={location}
+                    zIndex={1}
+                    fillColor={"rgba(255,0,255,0.2)"}
+                />
+                {lines.length > 0 && <Polyline coordinates={lines} zIndex={2}/>}
+
+
+            </MapView>
+            <View style={styles.coords_view}>
+                <Text style={styles.coords_text}> User position </Text>
+                <Text style={styles.coords_text}> {`latitude: ${location.latitude.toFixed(5)}`} </Text>
+                <Text style={styles.coords_text}> {`longitude: ${location.longitude.toFixed(5)}`} </Text>
+            </View>
+            <View style={styles.buttonbar}>
+                <Button
+                    onPress={drawLine}
+                    title="Draw"/>
+                <Button
+                    onPress={clearLine}
+                    title="Clear"/>
+                <Button
+                    onPress={clearMarker}
+                    title="Clear"/>
+            </View>
         </View>
     );
+
+
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height*0.9
-    }
+const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        title_map: {
+            textAlignVertical: "bottom",
+            flex: 0.1,
+            width: "100%",
 
-});
+        },
+        map: {
+            flex: 1,
+            width: Dimensions.get('window').width,
+            height: Dimensions.get('window').height * 0.9
+        },
+        coords_view: {
+            flex: 0.1,
+            width: "100%",
+            color: "rgba(100,100,100,1)",
+            textAlign: "left",
+            borderWidth: 1
+
+        },
+        coords_text: {
+            fontSize: 12,
+            width: "100%",
+            color: "rgba(100,100,100,1)",
+            textAlign: "left"
+
+        },
+
+
+        buttonbar: {
+            width: "100%",
+            flexDirection: "row",
+        },
+
+    }
+);
